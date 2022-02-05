2ba04598384402b5ae4c417658b3df8952b3e94f
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-02-05 20:12:18 +0500

    make and register TransactionDetails model for bankStatement app

--- src/bankingSystem/bankStatement/models.py
+++ src/bankingSystem/bankStatement/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from fundsTransfer.models import Accounts
 
 # BANK STATEMENT MODELS
+
+class TransactionDetails(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_no = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    tr_date = models.DateField(auto_now_add=False)
+    description =  models.CharField(max_length=50)
+    debit = models.DecimalField(max_digits=1000, decimal_places=2)
+    credit = models.DecimalField(max_digits=1000, decimal_places=2)
+    available_balance = models.DecimalField(max_digits=1000, decimal_places=2)
+
--- src/bankingSystem/fundsTransfer/models.py
+++ src/bankingSystem/fundsTransfer/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # FUNDS TRANSFER APP MODELS
+
+class Accounts(models.Model):
+    pass
