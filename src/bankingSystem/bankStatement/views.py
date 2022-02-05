f3e4169044819251a9652bb7b3ebcb09d96f72a3
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-02-05 21:40:10 +0500

    make incomplete checkbox selection form for generating the bank statement in the bankStatement app

--- src/bankingSystem/bankStatement/views.py
+++ src/bankingSystem/bankStatement/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from .models import TransactionDetails
 
 # BANK STATEMENT VIEWS
 
+def getStartDate(n):
+    return date.today() + relativedelta(months=-n)
+
 def index(request):
-    return render(request, 'bankStatement/index.html')
+    context = {}
+
+    if request=="POST":
+        transactions = TransactionDetails.objects.filter(user_id=request.user)
+        context = {'transactions':transactions}
+    
+    return render(request, 'bankStatement/index.html', context)
+
+
