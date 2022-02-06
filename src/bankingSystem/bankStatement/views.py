db47ef332f30e33e88ae6bd310119006116394e7
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-02-07 01:57:01 +0500

    make custom date feature in bankStatement app

--- src/bankingSystem/bankStatement/views.py
+++ src/bankingSystem/bankStatement/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from datetime import date
+from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 from .models import TransactionDetails
 
@@ -11,8 +11,21 @@ def getStartDate(n):
 def index(request):
     context = {}
     if request.method=="POST":
-        transactions = TransactionDetails.objects.filter(user_id = request.user)
-        context = {'transactions':transactions}
+        checkForCustomDate = request.POST['radio_month']
+
+        if checkForCustomDate=="True":
+            startDate = request.POST['startDate']
+            endDate = request.POST['endDate']
+            
+        else:
+            months = checkForCustomDate
+            startDate = getStartDate(int(months))
+            now = datetime.now()
+            endDate = now.strftime("%Y-%m-%d")
+
+        transactions = TransactionDetails.objects.filter(user_id = request.user, tr_date__gte=startDate, tr_date__lte=endDate)
+        transactions = transactions.order_by('tr_date')
+        context = {'transactions':transactions, 'date1':startDate, 'date2':endDate, 'something':checkForCustomDate}
     
     return render(request, 'bankStatement/index.html', context)
 
